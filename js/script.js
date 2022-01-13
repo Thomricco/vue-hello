7579c13f09992a0e9d0edb217ab8a0e11cbee465
Thomricco <tommasoricco@hotmail.it>
2022-01-13 14:50:06 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 new Vue({
     el: "#app",
-
+    data: {
+        message: 'ciao',
+        immagine: 'https://i.picsum.photos/id/444/300/300.jpg?hmac=KLAXKoCYVDuOGwIxLdKlqOUm7FsOz3BR2IwStGrJevQ',
+        messaggioUtente: '',
+    },
+    methods: {
+        saluta: function() {
+            alert('Benvenuto: ' + this.messaggioUtente)
+        }
+    }
 })
