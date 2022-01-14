0e8cc74718047638233b8b181b31cc051ac11d6e
Thomricco <tommasoricco@hotmail.it>
2022-01-14 11:02:48 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ new Vue({
     el: "#app",
     data: {
         message: 'ciao',
+        messageUser: '',
         immagine: 'https://i.picsum.photos/id/444/300/300.jpg?hmac=KLAXKoCYVDuOGwIxLdKlqOUm7FsOz3BR2IwStGrJevQ',
         messaggioUtente: '',
     },
     methods: {
         saluta: function() {
+
             alert('Benvenuto: ' + this.messaggioUtente);
             
-            this.message = prompt('inserisci una pass');
-        }
+            this.messageUser = prompt('inserisci una pass');
+
+            this.messaggioUtente = '"hai inviato:" '+ this.messageUser + ' " ';
+        },
     }
 })
